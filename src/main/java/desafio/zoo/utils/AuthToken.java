d12663125ff75c53d87f12c69c0955e18e3796d8
pescador95 <iedio_junior@hotmail.com>
2022-09-19 16:46:44 -0300

    Ajustado roles

--- src/main/java/desafio/zoo/utils/AuthToken.java
+++ src/main/java/desafio/zoo/utils/AuthToken.java
@@ -1,14 +1,12 @@
 package desafio.zoo.utils;
 
-import io.smallrye.jwt.build.Jwt;
 import desafio.zoo.model.Usuario;
+import io.smallrye.jwt.build.Jwt;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import javax.enterprise.context.ApplicationScoped;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.Arrays;
-import java.util.HashSet;
 
 @ApplicationScoped
 public class AuthToken {
@@ -19,7 +17,7 @@ public class AuthToken {
     public String GenerateAccessToken(Usuario pUsuario) {
         String accessToken = Jwt.issuer(this.issuer)
                 .upn(pUsuario.email)
-                .groups(new HashSet<>(Arrays.asList("veterinario", "biologo", "dev")))
+                .groups(String.valueOf(pUsuario.roleUsuario))
                 .expiresAt(Instant.now().plus(Duration.ofMinutes(10)))
                 .sign();
         return accessToken;

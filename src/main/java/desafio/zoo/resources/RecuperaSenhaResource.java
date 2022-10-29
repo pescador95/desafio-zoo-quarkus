8dd6d2c91d6eeb24c98d71962c33e780daeb919f
Iédio João Carabolante Júnior <iedio_junior@hotmail.com>
2022-10-29 12:40:24 -0300

    Implementado recuperação de senha via email

--- src/main/java/desafio/zoo/resources/RecuperaSenhaResource.java
+++ src/main/java/desafio/zoo/resources/RecuperaSenhaResource.java
@@ -2,8 +2,6 @@ package desafio.zoo.resources;
 
 import desafio.zoo.controller.RecuperaSenhaController;
 import desafio.zoo.model.Usuario;
-import io.quarkus.hibernate.orm.panache.PanacheQuery;
-import io.quarkus.panache.common.Page;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.security.RolesAllowed;
@@ -14,22 +12,31 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 import java.security.Principal;
-import java.util.List;
 
 @Path("/recuperaSenha")
 public class RecuperaSenhaResource {
     @Inject
     RecuperaSenhaController controller;
-    String retorno;
 
     @GET
     @Path("{email}")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes("application/json")
-    //@RolesAllowed({"veterinario", "biologo", "dev"})
     public Response sendMail(@PathParam("email") String email) {
         System.out.println("Sendmail function OK");
-        retorno = controller.enviaEmail(email);
-        return Response.ok(retorno).status(200).build();
+        controller.sendEmail(email);
+        return Response.ok().status(200).build();
+    }
+
+    @PUT
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes("application/json")
+    @RolesAllowed({"veterinario", "biologo", "dev"})
+    public Response update(Usuario pUsuario, @Context @NotNull SecurityContext context, @QueryParam("password") String password) {
+        Principal json = context.getUserPrincipal();
+        String email = json.getName();
+        controller.updatePassword(email, password);
+        return Response.ok().status(200).build();
     }
 }

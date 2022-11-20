0de62a1d7c36bf7acf3a57cff59f5e5ed1285384
Iédio João Carabolante Júnior <iedio_junior@hotmail.com>
2022-11-20 12:50:40 -0300

    corrigido constraint

--- src/main/java/desafio/zoo/model/HistoricoEtologico.java
+++ src/main/java/desafio/zoo/model/HistoricoEtologico.java
@@ -32,7 +32,7 @@ public class HistoricoEtologico extends PanacheEntityBase {
     @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
     public Date dataEtologico;
 
-    @Column(nullable = Boolean.FALSE)
+    @Column(nullable = false)
     public String nomeEtologico;
 
     @Column()

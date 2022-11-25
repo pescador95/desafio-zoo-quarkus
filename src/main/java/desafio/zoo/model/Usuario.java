2c58ab60da1ada9029ed10623659d2d194cc8417
Iédio João Carabolante Júnior <iedio_junior@hotmail.com>
2022-11-24 23:31:15 -0300

    corrigido import

--- src/main/java/desafio/zoo/model/Usuario.java
+++ src/main/java/desafio/zoo/model/Usuario.java
@@ -2,6 +2,7 @@ package desafio.zoo.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import io.quarkus.security.jpa.Password;
 import io.quarkus.security.jpa.Roles;
